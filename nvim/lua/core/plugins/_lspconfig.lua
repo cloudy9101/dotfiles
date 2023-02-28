eeabbe5fdbdc35880955a8a1e25ece77d01e45b2
Ray Hao <cloudy9101@gmail.com>
2023-02-28 22:07:43 +1300

    update nvim conf

--- nvim/lua/core/plugins/_lspconfig.lua
+++ nvim/lua/core/plugins/_lspconfig.lua
@@ -15,17 +15,28 @@ local servers = {
   gopls = {},
   html = {},
   jsonls = {},
-  solargraph = {},
-  sqlls = {},
-  sumneko_lua = {
+  lua_ls = {
     Lua = {
-      workspace = { checkThirdParty = false },
-      telemetry = { enable = false },
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
       diagnostics = {
-        globals = { 'vim' }
-      }
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
     },
   },
+  solargraph = {},
+  sqlls = {},
   tailwindcss = {},
   tsserver = {},
   vimls = {},
