681671e5d5da2e1be08bbd435308812664cc8ab4
Ray Hao <cloudy9101@gmail.com>
2023-06-06 21:58:45 +1200

    update install.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ if config checkout; then
   echo "Checked out config.";
 else
   echo "Backing up pre-existing dot files.";
-  config checkout 2>&1 | grep -E "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+  config checkout 2>&1 | grep -E "\s+\." | awk {'print $1'} | xargs -I{} mv $HOME/{} $HOME/.config-backup/{}
 fi;
 config checkout
 config config status.showUntrackedFiles no
