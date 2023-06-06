f764ea203098f1c06bd8a5d03c0fc1dd8e56217c
Ray Hao <cloudy9101@gmail.com>
2023-06-06 21:30:31 +1200

    update install.sh

--- install.sh
+++ install.sh
@@ -12,4 +12,16 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM
 
 # clone dotfiles in $HOME
 git clone --bare git@github.com:cloudy9101/dotfiles.git --branch bare "$HOME/.cfg"
-git --git-dir="$HOME/.cfg/" --work-tree="$HOME" checkout
+
+function config {
+   /usr/bin/git --git-dir="$HOME/.cfg/" --work-tree="$HOME" "$@"
+}
+mkdir -p .config-backup
+if config checkout; then
+  echo "Checked out config.";
+else
+  echo "Backing up pre-existing dot files.";
+  config checkout 2>&1 | grep -E "\s+\." | awk 'print $1' | xargs -I{} mv {} .config-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
