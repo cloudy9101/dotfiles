81bb99bbc56895ce5269686601e33c07a9539faa
Ray Hao <cloudy9101@gmail.com>
2024-04-12 19:58:55 +1200

    update

--- .path.zsh
+++ .path.zsh
@@ -1,14 +1,5 @@
 # Homebrew
 [[ -f /opt/homebrew/bin/brew ]] && eval "$(/opt/homebrew/bin/brew shellenv)"
 
-# Nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
-
-# Yarn
-PATH=$HOME/.yarn/bin:$PATH
-
-# rbenv
-which rbenv &> /dev/null
-[[ $? -eq 0 ]] && eval "$(rbenv init - zsh)"
+# asdf
+. $(brew --prefix asdf)/libexec/asdf.sh
