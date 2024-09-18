067d4442c188aee60a7653635de4ba8d5f753ac5
Ray Hao <cloudy9101@gmail.com>
2024-09-18 15:46:42 +1200

    update

--- install.sh
+++ install.sh
@@ -38,50 +38,17 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
   brew install neovim tmux ripgrep starship asdf fzf
 fi
 
-if [ ! -d "$HOME/.cfg" ]; then
-  if [ ! -f "$HOME/.ssh/known_hosts" ]; then
-    mkdir -p $HOME/.ssh
-    touch $HOME/.ssh/known_hosts
-  fi
-  
-  echo "github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl" >> $HOME/.ssh/known_hosts
-  echo "github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=" >> $HOME/.ssh/known_hosts
-  echo "github.com ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj7ndNxQowgcQnjshcLrqPEiiphnt+VTTvDP6mHBL9j1aNUkY4Ue1gvwnGLVlOhGeYrnZaMgRK6+PKCUXaDbC7qtbW8gIkhL7aGCsOr/C56SJMy/BCZfxd1nWzAOxSDPgVsmerOBYfNqltV9/hWCqBywINIR+5dIg6JTJ72pcEpEjcYgXkE2YEFXV1JHnsKgbLWNlhScqb2UmyRkQyytRLtL+38TGxkxCflmO+5Z8CSSNY7GidjMIZ7Q4zMjA2n1nGrlTDkzwDCsw+wqFPGQA179cnfGWOWRVruj16z6XyvxvjJwbz0wQZ75XK5tKSb7FNyeIEs4TT4jk+S4dhPeAUC5y+bDYirYgM4GC7uEnztnZyaVWQ7B381AK4Qdrwt51ZqExKbQpTUNn+EjqoTwvqNj4kqx5QUCI0ThS/YkOxJCXmPUWZbhjpCg56i+2aB6CmK2JGhn57K5mj0MNdBXA4/WnwH6XoPWJzK5Nyu2zB3nAZp+S5hpQs+p1vN1/wsjk=" >> $HOME/.ssh/known_hosts
-
-  # clone dotfiles in $HOME
-  git clone --bare git@github.com:cloudy9101/dotfiles.git --branch bare "$HOME/.cfg"
-
-  function config {
-    git --git-dir="$HOME/.cfg/" --work-tree="$HOME" "$@"
-  }
-
-  if config checkout; then
-    echo "Checked out config.";
-  else
-    echo "Backing up pre-existing dot files.";
-
-    mkdir -p "$HOME/.config-backup"
-    config checkout 2>&1 | grep -E "\s+\." | awk {'print $1'} | xargs -I{} mv $HOME/{} $HOME/.config-backup/{}
-  fi;
-
-  config config status.showUntrackedFiles no
-fi
+# Link config files
+ln -s ./config/git/gitconfig $HOME/.gitconfig
+ln -s ./config/git/gitignore $HOME/.gitignore
+ln -s ./config/zsh/zshenv $HOME/.zshenv
+ln -s ./config/zsh $XDG_CONFIG_HOME/zsh
+ln -s ./config/tmux/tmux.conf $HOME/.tmux.conf
+ln -s ./config/asdf/asdfrc $HOME/.asdfrc
+ln -s ./config/asdf/tool-versions $HOME/.tool-versions
 
 # Install tpm
 if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+  mkdir -p $HOME/.tmux/plugins
   git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
-fi
-
-. $HOME/.path.zsh
-
-# Setup nodejs/golang asdf plugin and install global version
-if [ ! -d "$HOME/.asdf/plugins/nodejs" ]; then
-  asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-  asdf install nodejs 20.16.0
-  asdf global nodejs 20.16.0
-fi
-if [ ! -d "$HOME/.asdf/plugins/golang" ]; then
-  asdf plugin add golang https://github.com/asdf-community/asdf-golang.git
-  asdf install golang latest
-  asdf global golang latest
 fi
