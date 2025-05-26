834c24153e3f8c25c2fc845463c8af052c2dc767
Ray Hao <cloudy9101@gmail.com>
2025-05-26 23:54:34 +1200

    update

--- config/zsh/ohmyzsh-custom/alias.zsh
+++ config/zsh/ohmyzsh-custom/alias.zsh
@@ -1,3 +1,7 @@
 alias vi='nvim'
 alias dot='cd $HOME/Projects/dotfiles'
 alias crt='cd $(git rev-parse --show-toplevel)'
+
+alias tls='tmux list-sessions'
+alias tns='tmux new-session -s'
+alias tas='tmux attach-session'
