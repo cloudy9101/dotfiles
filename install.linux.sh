090a4450bce6fb04e9138b67d04f686a9cf385e2
Ray Hao <cloudy9101@gmail.com>
2026-07-03 21:47:05 +1200

    update

--- install.linux.sh
+++ install.linux.sh
@@ -7,6 +7,9 @@ sudo apt-get install -y build-essential procps curl file git
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+echo "brew install"
+brew bundle check --file ./config/Brewfile || brew bundle install --file ./config/Brewfile
+
 link_config_files
 
 install_ohmyzsh
