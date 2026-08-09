d59d4ca4e835b8034005a12e214939068f9110af
Ray Hao <hirayhao@gmail.com>
2026-08-09 22:19:45 +1200

    treesitter

--- config/nvim/lua/treesitter.lua
+++ config/nvim/lua/treesitter.lua
@@ -1,7 +1,27 @@
+-- Register languages for treesitter
+local filetypes = {
+  bash = { 'sh' },
+  embedded_template = { 'eruby' },
+  javascript = { 'javascriptreact', 'ecma', 'ecmascript', 'jsx', 'js' },
+  json = { 'jsonc' },
+  python = { 'py' },
+  tsx = { 'typescriptreact', 'typescript.tsx' },
+  typescript = { 'ts' },
+}
+
+for lang, ft in pairs(filetypes) do
+  vim.treesitter.language.register(lang, ft)
+end
+
 -- Start Treesitter if there's a parser
 vim.api.nvim_create_autocmd('FileType', {
   callback = function(ev)
-    if vim.treesitter.language.add(ev.match) then
+    local lang = vim.treesitter.language.get_lang(ev.match)
+    if not lang then
+      return
+    end
+
+    if vim.treesitter.language.add(lang) then
       pcall(vim.treesitter.start)
     end
   end
